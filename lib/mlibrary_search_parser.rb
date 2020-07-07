1a2b89f22bf527ca89a0d47790415c5e2c61e301
Anthony Thomas <esty@umich.edu>
2020-07-07 12:29:02 -0400

    Basic AND/OR handling + specs

--- lib/mlibrary_search_parser.rb
+++ lib/mlibrary_search_parser.rb
@@ -1,101 +1,113 @@
-require "mlibrary_search_parser/version"
 require 'parslet'
 
 module MLibrarySearchParser
   class Error < StandardError; end
 
 
-  include Parslet
+  class QueryParser < Parslet::Parser
 
-  ###################################
-  # BASICS
-  ###################################
-  rule(:empty_string) { str("") }
-  rule(:lparen) { str('(') >> space? }
-  rule(:rparen) { space? >> str(')') }
+    ###################################
+    # BASICS
+    ###################################
+    rule(:empty_string) { str("") }
+    rule(:lparen) { str('(') >> space? }
+    rule(:rparen) { space? >> str(')') }
 
-  rule(:space) { match['\\s'].repeat(1) }
-  rule(:space?) { space.maybe }
+    rule(:space) { match['\\s'].repeat(1) }
+    rule(:space?) { space.maybe }
 
-  rule(:dquote) { str('"') }
-  rule(:squote) { str("'") }
-  rule(:nondquote) { match['^"'].repeat(1) }
-  rule(:nonsquote) { match["^'"].repeat(1) }
+    rule(:dquote) { str('"') }
+    rule(:squote) { str("'") }
+    rule(:nondquote) { match['^"'].repeat(1) }
+    rule(:nonsquote) { match["^'"].repeat(1) }
 
-  ##################################################
-  # "Smart" quotes and other near-miss characters
-  ##################################################
+    ##################################################
+    # "Smart" quotes and other near-miss characters
+    ##################################################
 
-  rule(:smart_dash) { str("\u2013") | str("\u2014") | str("\u2015") }
+    rule(:smart_dash) { str("\u2013") | str("\u2014") | str("\u2015") }
 
-  rule(:smart_squote) { str("\u2018") | str("\u2019") |
-    str("\u201b") | str("\u2032") }
+    rule(:smart_squote) { str("\u2018") | str("\u2019") |
+      str("\u201b") | str("\u2032") }
 
-  rule(:smart_underscore) { str("\u2017") }
+    rule(:smart_underscore) { str("\u2017") }
 
-  rule(:smart_comma) { str("\u201a") }
+    rule(:smart_comma) { str("\u201a") }
 
-  rule(:smart_dquote) { str("\u201c") | str("\u201d") |
-    str("\u201e") | str("\u2033") }
+    rule(:smart_dquote) { str("\u201c") | str("\u201d") |
+      str("\u201e") | str("\u2033") }
 
-  rule(:smartquote) { smart_squote | smart_dquote }
+    rule(:smartquote) { smart_squote | smart_dquote }
 
 
 
-  ###################################
-  # Phrase: Double-quoted strings
-  ###################################
-  # Phrases can have anything in them except a double-quote character
+    ###################################
+    # Phrase: Double-quoted strings
+    ###################################
+    # Phrases can have anything in them except a double-quote character
 
-  rule(:phrase) { dquote >> nondquote.repeat(1) >> dquote }
+    rule(:phrase) { dquote >> nondquote.repeat(1) >> dquote }
 
-  ###################################
-  # Words
-  ###################################
-  #  A word, in this case, is a string of characters that doesn't
-  # include any of the reserved characters (,), or ", or a space
+    ###################################
+    # Words
+    ###################################
+    #  A word, in this case, is a string of characters that doesn't
+    # include any of the reserved characters (,), or ", or a space
 
-  rule(:word_char) { match['^\(\)\"\s'] }
-  rule(:word) { word_char.repeat(1) }
+    rule(:word_char) { match['^\(\)\"\s'] }
+    rule(:word) { word_char.repeat(1) }
 
-  ###################################
-  # Tokens
-  ###################################
-  # A token is a logic word-unit. So, either a word, or a phrase
-  # that we treat as a single "word"
+    ###################################
+    # Tokens
+    ###################################
+    # A token is a logic word-unit. So, either a word, or a phrase
+    # that we treat as a single "word"
 
-  rule(:token) { phrase | word }
-  rule(:tokens) { any_op.absent? >> token >> (space >> tokens).repeat(0) }
+    rule(:token) { phrase | word }
+    rule(:tokens) { any_op.absent? >> token >> (space >> tokens).repeat(0) }
 
-  #######################################
-  # BINARY OPERATORS
-  # ####################################
+    #######################################
+    # BINARY OPERATORS
+    # ####################################
 
-  rule(:and_op) { space? >> str('AND') >> space }
-  rule(:or_op) { space? >> str('OR') >> space }
+    rule(:and_op) { space? >> str('AND') >> space }
+    rule(:or_op) { space? >> str('OR') >> space }
 
-  #######################################
-  # UNARY OPERATORS
-  #####################################
+    #######################################
+    # UNARY OPERATORS
+    #####################################
 
-  rule(:not_op) { space? >> str('NOT')  >> space }
+    rule(:not_op) { space? >> str('NOT')  >> space }
 
-  rule(:any_op) { and_op | or_op | not_op }
+    rule(:any_op) { and_op | or_op | not_op }
 
-  #######################################
-  # BASIC OPERATOR EXPRESSIONS
-  #####################################
-  # These include the normal booleans and NOT, where we
-  # have spaces around them
-  #
-  # No fieldeds are allowed here
+    #######################################
+    # BASIC OPERATOR EXPRESSIONS
+    #####################################
+    # These include the normal booleans and NOT, where we
+    # have spaces around them
+    #
+    # No fieldeds are allowed here
 
-  rule(:parens) { lparen >> or_expr >> rparen | tokens }
-  rule(:not_expr) { not_op >> parens.as(:not) | parens }
-  rule(:and_expr) { (not_expr.as(:left) >> and_op >> and_expr.as(:right)).as(:and) | not_expr }
-  rule(:or_expr) { (and_expr.as(:left) >> or_op >> or_expr.as(:right)).as(:or) | and_expr }
+    rule(:parens) { lparen >> or_expr >> rparen | tokens }
+    rule(:not_expr) { not_op >> parens.as(:not) | parens }
+    rule(:and_expr) { (not_expr.as(:left) >> and_op >> and_expr.as(:right)).as(:and) | not_expr }
+    rule(:or_expr) { (and_expr.as(:left) >> or_op >> or_expr.as(:right)).as(:or) | and_expr }
 
-  rule(:bare_expr) { (or_expr >> not_expr.repeat(0)) }
+    rule(:bare_expr) { (or_expr >> not_expr.repeat(0)) }
 
-  rule(:search) { space? >> (bare_expr.repeat(0)).as(:search) >> space? }
+    rule(:search) { space? >> (bare_expr.repeat(0)).as(:search) >> space? }
+    root(:search)
+  end
+
+  class QueryTransformer < Parslet::Transform
+    rule(:and => { :left => simple(:l), :right => simple(:r) } ) {
+      "(#{l}) AND (#{r})"
+    }
+    rule(:or => { :left => simple(:l), :right => simple(:r) } ) {
+      "(#{l}) OR (#{r})"
+    }
+    rule(:search => simple(:s)) { s.to_s }
+    rule(:search => sequence(:s)) { s.join(" ") }
+  end
 end
--- spec/mlibrary_search_parser_spec.rb
+++ spec/mlibrary_search_parser_spec.rb
@@ -3,7 +3,33 @@ RSpec.describe MLibrarySearchParser do
     expect(MLibrarySearchParser::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  before(:example) do
+    @parser = MLibrarySearchParser::QueryParser.new
+    @transformer = MLibrarySearchParser::QueryTransformer.new
+  end
+
+  it "returns a plain search" do
+    parsed = @parser.parse("A search")
+    expect(@transformer.apply(parsed)).to eq "A search"
+  end
+
+  it "ignores lower-case and" do
+    parsed = @parser.parse("mark twain and huck finn")
+    expect(@transformer.apply(parsed)).to eq "mark twain and huck finn"
+  end
+
+  it "demarcates AND left/right scopes" do
+    parsed = @parser.parse("mark twain AND huck finn")
+    expect(@transformer.apply(parsed)).to eq "(mark twain) AND (huck finn)"
+  end
+
+  it "ignores lower-case or" do
+    parsed = @parser.parse("mark twain or huck finn")
+    expect(@transformer.apply(parsed)).to eq "mark twain or huck finn"
+  end
+
+  it "demarcates OR left/right scopes" do
+    parsed = @parser.parse("mark twain OR huck finn")
+    expect(@transformer.apply(parsed)).to eq "(mark twain) OR (huck finn)"
   end
 end
