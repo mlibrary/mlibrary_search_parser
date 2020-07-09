2a01212578cb2d54b8197080083fa920aff9a1b3
Anthony Thomas <esty@umich.edu>
2020-07-09 13:17:51 -0400

    Tests for pre-query double quote parser

---
+++ spec/prequery_dquote_parser_spec.rb
@@ -0,0 +1,21 @@
+RSpec.describe "PreQueryDoubleQuotesParser" do
+  before do
+    @parser = MLibrarySearchParser::PreQueryDoubleQuotesParser.new
+  end
+
+  it "validates balanced double quotes" do
+    expect { @parser.parse("\"test\"") }.not_to raise_error
+  end
+
+  it "validates an arbitrary even number of quotes" do
+    expect { @parser.parse("t\"e \"sst tes\"\"") }.not_to raise_error
+  end
+
+  it "fails on an unmatched quote" do
+    expect { @parser.parse("\"") }.to raise_error(Parslet::ParseFailed)
+  end
+
+  it "fails on an odd number of quotes" do
+    expect { @parser.parse("\"test\"\" thing") }.to raise_error(Parslet::ParseFailed)
+  end
+end
