62e0a838dff7b13b9a54e9fbc9fcbd12d5806b6e
Anthony Thomas <esty@umich.edu>
2020-07-17 09:14:33 -0400

    Include prequery nested field parser in search handler preprocess

--- spec/mlibrary_search_handler_spec.rb
+++ spec/mlibrary_search_handler_spec.rb
@@ -28,5 +28,20 @@ RSpec.describe "MLibrarySearchHandler" do
       output = @handler.pre_process("\"a \"search\"\"")
       expect(output).to eq "\"a \"search\"\""
     end
+
+    it "removes immediately-nested fields" do
+      output = @handler.pre_process("title:author:blah")
+      expect(output).to eq "title:author blah"
+    end
+
+    it "removes fields nested in parentheses" do
+      output = @handler.pre_process("title:(thing author:blah")
+      expect(output).to eq "title:thing author:blah"
+    end
+
+    it "removes fields differently nested in parentheses" do
+      output = @handler.pre_process("title:(author:thing)")
+      expect(output).to eq "title:author thing"
+    end
   end
 end
