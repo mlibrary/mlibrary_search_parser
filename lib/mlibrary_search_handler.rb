e3ecf5fc02e917746085ee278ed19df4dd4b8bd6
Anthony Thomas <esty@umich.edu>
2020-07-17 09:17:33 -0400

    Clarify some of the search handler functionality

--- lib/mlibrary_search_handler.rb
+++ lib/mlibrary_search_handler.rb
@@ -16,11 +16,14 @@ module MLibrarySearchParser
       begin
         PreQueryNestedFieldsParser.new.parse(search)
       rescue Parslet::ParseFailed
+        # The nested fields parser is only good at recognizing
+        # fields that are explicitly nested using parentheses,
+        # so we remove the parentheses
         search = search.gsub(/(.*):\((.*):(.*)\)/, '\1:\2:\3')
       end
 
       # We want to eliminate nested fields like author:title:blah
-      # They are inordinately hard to recognize/prevent with Parslet
+      # They are unreasonably hard to recognize/prevent with Parslet
       search = search.gsub(/(.*):([^\s]*):(.*)/, '\1:\2 \3')
       search
     end
