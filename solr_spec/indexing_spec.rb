c34fa078269f4e08047721d388bd77ae112ae645
Anthony Thomas <esty@umich.edu>
2020-09-14 16:59:08 -0400

    indexing spec pulls in docs

--- solr_spec/indexing_spec.rb
+++ solr_spec/indexing_spec.rb
@@ -2,16 +2,20 @@ require 'solr_wrapper'
 require 'simple_solr_client'
 require 'pry-byebug'
 require_relative 'ssc_client_monkeypatch'
+require_relative 'test_solr_docs'
 
-RSpec.describe "ping" do
-  it "can see solr" do
+RSpec.describe "indexing" do
+  it "finds the right docs when searching" do
     SolrWrapper.wrap do |solr|
       SOLR_INSTANCE_DIR = solr.instance_dir
-      # binding.pry
       client = SimpleSolrClient::Client.new(solr.url)
       client.new_core('test_core') unless client.cores.include?('test_core')
       core = client.core('test_core')
-      expect(core.up?).to be true
+      # binding.pry
+      core.add_docs(TestDocs::DOCS)
+      core.commit
+      expect(core.number_of_documents).to eq 45
+      # find 2, not 3
     end
   end
 end
