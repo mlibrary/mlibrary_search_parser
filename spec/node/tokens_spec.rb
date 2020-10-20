3ac1bafbf31779fb28a0b960678b401d6381cb23
Bill Dueber <bill@dueber.com>
2020-10-20 15:21:05 -0400

    Add comparison, stringify, and traversal methods
    This is enormous, and adds a ton of stuff while changing where a lot of it lives.
    Most of this was necessary to give me the tools I need to build up the solr
    queries without having to do violence to the tree along the way.
    
    The highlights are:
      * #to_clean_string for all nodes, that guess when to use parens or not
      * #== (equality) for all node types
      * #node_type and #is_type?(:type) for easy identification of node types
      * #ancestors / #descendants to get lists of nodes
      * flatten to get a flat list of the whole tree from this node
      * #tokens_string to get a list of the tokens used without all the parens/booleans/whatever
      * Added @payload to the base node -- a hash where we can stuff things
      * Added a new node type: EmptyNode
      * #deep_dup, which produces a deep duplicate with all new objects.
        * optionally takes a block that gets a node and returns another node, so you can
          change the duplicated tree.
      * #trim, which takes a block that returns a boolean to determine which nodes
        to trim off the tree
      * #shake, which does some *very* simple tree-shaking, e.g., NOT(NOT a) -> a,
        (title:one AND title:two) -> title:(one AND two). I'm not sure we're going to
        bother with it
      * So many new tests
        * helpers to create nodes of various types (see `spec_helper.rb`)

---
+++ spec/node/tokens_spec.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+require 'spec_helper'
+require "mlibrary_search_parser/node/tokens"
+
+RSpec.describe "TokensNode" do
+  before do
+    @node = MLibrarySearchParser::Node::TokensNode.new("some text")
+  end
+
+  it "has text" do
+    expect(@node.text).to eq "some text"
+  end
+
+  it "returns that text for to_s" do
+    expect(@node.to_s).to eq "some text"
+  end
+
+  it "returns that text for to_webform" do
+    expect(@node.to_webform).to eq({"query" => "some text"})
+  end
+
+  it "parenthesizes multiple words" do
+    expect(@node.to_clean_string).to eq "(some text)"
+  end
+
+  it "recognizes equality of itself" do
+    expect(@node).to eq(@node)
+  end
+
+  it "implements equality" do
+    expect(tnode("one")).to eq(tnode('one'))
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,3 +12,37 @@ RSpec.configure do |config|
     c.syntax = :expect
   end
 end
+
+def nodeify(node_or_string)
+  if node_or_string.kind_of? MLibrarySearchParser::Node::BaseNode
+    node_or_string
+  else
+    MLibrarySearchParser::Node::TokensNode.new(node_or_string)
+  end
+end
+
+def tnode(node_or_string)
+  nodeify(node_or_string)
+end
+
+def and_node(node_or_string1, node_or_string2)
+  MLibrarySearchParser::Node::AndNode.new(nodeify(node_or_string1), nodeify(node_or_string2))
+end
+
+def or_node(node_or_string1, node_or_string2)
+  MLibrarySearchParser::Node::OrNode.new(nodeify(node_or_string1), nodeify(node_or_string2))
+end
+
+def fielded_node(field, node_or_string)
+  MLibrarySearchParser::Node::FieldedNode.new(field, nodeify(node_or_string))
+end
+
+def not_node(node_or_string)
+  MLibrarySearchParser::Node::NotNode.new(nodeify(node_or_string))
+end
+
+TEST_HANDLER =  MLibrarySearchParser::SearchHandler.new('spec/data/fields_file.json')
+
+def search_node(*clauses)
+  MLibrarySearchParser::Node::SearchNode.new(clauses.map{|c| nodeify(c)})
+end
