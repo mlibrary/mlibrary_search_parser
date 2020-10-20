20efc4b66ded9a08e23d1e31ed19906565b5e68b
Bill Dueber <bill@dueber.com>
2020-10-20 15:21:05 -0400

    Add comparison, stringify, and traversal methods
    This is enormous, and adds a ton of stuff while changing where a lot of it lives.
    Most of this was necessary to give me the tools I need to build up the solr
    queries without having to do violence to the tree along the way.
    
    The highlights are:
      * #to_clean_string for all nodes, that guess when to use parens or not
      * #== (equality) for all node types
      * #node_type and #is_type?(:type) for easy identification of node types
      * #ancestors / #descendants to get lists of nodes
      * flatten to get a flat list of the whole tree from this node
      * #tokens_string to get a list of the tokens used without all the parens/booleans/whatever
      * Added @payload to the base node -- a hash where we can stuff things
      * Added a new node type: EmptyNode
      * #deep_dup, which produces a deep duplicate with all new objects.
        * optionally takes a block that gets a node and returns another node, so you can
          change the duplicated tree.
      * #trim, which takes a block that returns a boolean to determine which nodes
        to trim off the tree
      * #shake, which does some *very* simple tree-shaking, e.g., NOT(NOT a) -> a,
        (title:one AND title:two) -> title:(one AND two). I'm not sure we're going to
        bother with it
      * So many new tests
        * helpers to create nodes of various types (see `spec_helper.rb`)

---
+++ spec/node/fielded_spec.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe "FieldedNode" do
+  before do
+    tokens = tnode("some terms")
+    @node  = MLibrarySearchParser::Node::FieldedNode.new("title", tokens)
+  end
+
+  it "has to_s" do
+    expect(@node.to_s).to eq "title:(some terms)"
+  end
+
+  it "has a simple to_clean_string" do
+    expect(fielded_node('title', 'one').to_clean_string).to eq("title:one")
+  end
+
+  it "has multi-term to_clean_string" do
+    expect(@node.to_clean_string).to eq("title:(some terms)")
+  end
+
+  it "has a more complex to_clean_string" do
+    n = and_node(fielded_node('title', and_node('one', 'two')), fielded_node("author", '"phrase here"'))
+    expect(n.to_clean_string).to eq('title:(one AND two) AND author:("phrase here")')
+  end
+
+  it "has to_webform" do
+    expect(@node.to_webform).to eq([
+                                       {"field" => "title"},
+                                       {"query" => "some terms"}
+                                   ])
+  end
+
+  describe "Equality and traversing" do
+    it "implements ==" do
+      n1 = and_node(fielded_node('title', 'one'), fielded_node('author', 'two'))
+      n2 = and_node(fielded_node('title', 'one'), fielded_node('author', 'two'))
+      n3 = and_node(fielded_node('title', 'one'), fielded_node('author', 'XXXXXXX'))
+
+      expect(n1).to eq(n2)
+      expect(n2).to_not eq(n3)
+    end
+
+    it "can deep-dup" do
+      n1 = and_node(fielded_node('title', or_node('one', 'two')), "three")
+      expect(n1.dup).to eq(n1)
+    end
+  end
+end
