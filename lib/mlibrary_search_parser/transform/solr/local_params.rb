5872789697f4ded78ec5bdb6bf9436cca54e7e7e
Bill Dueber <bill@dueber.com>
2020-10-25 14:45:33 -0400

    What am I doing

---
+++ lib/mlibrary_search_parser/transform/solr/local_params.rb
@@ -0,0 +1,37 @@
+require_relative 'json_edismax'
+require 'uri'
+
+module MLibrarySearchParser
+  module Transformer
+    module Solr
+      class LocalParams < JsonEdismax
+
+        # @override
+        def edismaxify(node, field: :allfields, extras: {}, escape: false)
+          v = URI.encode_www_form_component(lucene_escape_node(node).to_clean_string)
+          v = lucene_escape_node(node).to_clean_string
+          "{!edismax qf=\"#{field}\" v=\"#{v}\"}"
+        end
+
+
+        # @override
+        def boolnode(node, shouldmust)
+
+          joiner = if shouldmust == :must
+                     "AND"
+                   elsif shouldmust == :should
+                     "OR"
+                   else
+                     raise "ShouldMust should only get :should or :must"
+                   end
+          "(#{_transform(node.left)} #{joiner} #{_transform(node.right)})"
+        end
+
+        def transform_not_node(node, extras: {})
+          "NOT (#{_transform(node)})"
+        end
+
+      end
+    end
+  end
+end
