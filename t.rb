182bf9438cb6f0a9c31be6d18fb5eda549e53254
Bill Dueber <bill@dueber.com>
2020-10-26 11:27:57 -0400

    More config in t.rb

--- t.rb
+++ t.rb
@@ -7,10 +7,18 @@ require 'erb'
 @config = YAML.load(ERB.new(File.read(@config_file)).result)
 
 
-# Your solr port
-port = 9639
-client = SimpleSolrClient::Client.new(port)
+# Your solr port (local) or full URL (remote)
+portOrURL = 'http://julep-1.umdl.umich.edu:8026/solr'
+# portOrURL = 9639
+client = SimpleSolrClient::Client.new(portOrURL)
 @core = client.core(client.cores.first)
+
+# Other solr params
+#
+@solr_params = {
+    fl: 'id,title,mainauthor',
+    rows: 10
+}
 
 # Index a document, translating title to title_common and
 # setting a random id if needed and the allfields
@@ -33,14 +41,15 @@ def lp(s)
 end
 
 # Make the solr call with the localparams and get the docs
-def get(str)
-  resp = get_respose(str)
+# Override solr parms in kwargs
+def get(str, **kwargs)
+  resp = get_respose(str, **kwargs)
   resp['response']['docs']
 end
 
 # In case we need to look at the whole response
-def get_respose(str)
+def get_respose(str, **kwargs)
   s = search(str)
-  params = lp(s).params
+  params = lp(s).params.merge(@solr_params).merge(kwargs)
   @core.get('select', params)
 end
