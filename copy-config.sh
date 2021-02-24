cbd702b7babcb00fac24c95dcbc13ce505393a2d
Esty Thomas <esty@umich.edu>
2021-02-24 11:10:09 -0500

    broke my own script. fixed it.

--- copy-config.sh
+++ copy-config.sh
@@ -1,14 +1,15 @@
 #! /bin/bash
 
 K8S_LOCATION="../../kubernetes-config/base/search/spectrum-config"
-SPECTRUM_CONFIG_LOCATION="../spectrum/config/"
-PARSER_TEST_CONFIG_LOCATION="./spec/data/"
+SPECTRUM_CONFIG_LOCATION="../spectrum/config"
+PARSER_TEST_CONFIG_LOCATION="./spec/data"
 BRANCH="parser-test-1"
 
 cp $K8S_LOCATION/$BRANCH/config--foci--00-catalog.yml $PARSER_TEST_CONFIG_LOCATION/00-catalog.yml
 
 for f in $K8S_LOCATION/common/config--*
 do
+  #echo $f
   if [[ $f =~ ^.*foci.*$ ]]; then
     new=`echo $f | awk -F'--' '{print $3}'`
     cp $f $SPECTRUM_CONFIG_LOCATION/foci/$new
@@ -16,17 +17,18 @@ do
     new=`echo $f | awk -F'--' '{print $2}'`
     cp $f $SPECTRUM_CONFIG_LOCATION/$new
   fi
+  #echo $new
 done
 
 for f in $K8S_LOCATION/$BRANCH/config--*
 do
-  echo $f
+  #echo $f
   if [[ $f =~ ^.*foci.*$ ]]; then
     new=`echo $f | awk -F'--' '{print $3}'`
-    cp $f $CONFIG_LOCATION/foci/$new
+    cp $f $SPECTRUM_CONFIG_LOCATION/foci/$new
   else
     new=`echo $f | awk -F'--' '{print $2}'`
-    cp $f $CONFIG_LOCATION/$new
+    cp $f $SPECTRUM_CONFIG_LOCATION/$new
   fi
-  echo $new
+  #echo $new
 done
